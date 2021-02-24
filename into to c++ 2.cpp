41d1373af44de50e6ffb7d7b23cf0ad261d2dab6
Suri-123 <71787536+Suri-123@users.noreply.github.com>
2021-02-24 21:41:11 +0430

    Add files via upload

---
+++ into to c++ 2.cpp
@@ -0,0 +1,16 @@
+//c++ variable declaration and initialization.
+using namespace std;
+#include<iostream>
+int main(){
+int a,b,c;
+a=40;
+b=20;
+c=a+b;
+cout<<c<<endl;
+c=a-b;
+cout<<c<<endl;
+c=a*b;
+cout<<c<<endl;
+c=a/b;
+cout<<c<<endl;
+}
